3bb443a841ca421645f32126f305111c89c37d32
Ananya Jain <ananya.jain@klearai.com>
2025-07-14 04:27:12 +0530

    changed langchain_openai to langchain.chat_models

--- MedTalk_Chatbot_withAgenticAI_Streamlit_v5.py
+++ MedTalk_Chatbot_withAgenticAI_Streamlit_v5.py
@@ -80,7 +80,8 @@ import pandas as pd
 import io
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import ChatOpenAI
+#from langchain_openai import ChatOpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.schema import Document
