f93b0c33d8e53281daf2e4cabd8951c1328c6e15
Ananya Jain <ananya.jain@klearai.com>
2025-07-14 05:06:29 +0530

    changed importing OpenAIEmbeddings line

--- MedTalk_Chatbot_withAgenticAI_Streamlit_v5.py
+++ MedTalk_Chatbot_withAgenticAI_Streamlit_v5.py
@@ -83,7 +83,8 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 #from langchain_openai import ChatOpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import OpenAIEmbeddings
+#from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain.schema import Document
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
